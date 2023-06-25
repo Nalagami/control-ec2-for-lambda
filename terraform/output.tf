81ce7363d17ba86ee021c09419ed6674b40c0151
nalagami <tnt547@gmail.com>
2023-06-25 23:24:20 +0900

    add terraform lambda

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,3 +1,7 @@
 output "ec2_global_ips" {
-    value = "${aws_instance.handson_ec2.*.public_ip}"
+  value = aws_instance.handson_ec2.*.public_ip
+}
+
+output "lambda_function_URLs" {
+  value = aws_lambda_function_url.endpoint.function_url
 }
